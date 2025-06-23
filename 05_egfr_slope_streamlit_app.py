ecbb04fe24791cd8c74c32e1e50a4708d7a3d647
jyjungmd <jyjungmd@gmail.com>
2025-06-23 23:46:46 +0900

    Add files via upload

--- 05_egfr_slope_streamlit_app.py
+++ 05_egfr_slope_streamlit_app.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 # 모델 import
 
-from google.colab import files
+# from google.colab import files
 
 print("Please upload the 'egfr_slope_model.pkl' file.")
 uploaded = files.upload()
@@ -65,4 +65,4 @@ if st.button("예측하기"):
     shap.plots.waterfall(shap_values[0], show=False)
     st.pyplot(bbox_inches='tight')
 
-%pip install streamlit
+# %pip install streamlit
